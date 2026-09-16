59c51807195b928a58bcc22cad39f454ddbc6a76
alexsandrogoes7-png <alexsandrogoes7@gmail.com>
2026-09-16 19:34:05 -0300

    Update

--- modal.js
+++ modal.js
@@ -10,11 +10,15 @@ const modal = getElement('.modal');
 const openModal = () => {
     container.classList.remove('opacity-0','pointer-events-none')
     container.classList.add('opacity-100','pointer-events-auto')
+    modal.classList.remove('pointer-events-none')
+    modal.classList.add('pointer-events-auto')
 };
 
 const closeModal = () =>{
     container.classList.remove('opacity-100','pointer-events-auto');
     container.classList.add('opacity-0','pointer-events-none');
+    modal.classList.remove('pointer-events-auto')
+    modal.classList.add('pointer-events-none')
 };
 
 button.addEventListener('click',()=>{
